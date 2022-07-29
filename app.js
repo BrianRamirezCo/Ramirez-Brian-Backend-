2609c59b7beaa93798db641f593f32a73aecd084
Ramirez Brian <tiziano0705@hotmail.com>
2022-07-28 22:08:49 -0300

    2

--- app.js
+++ app.js
@@ -24,23 +24,37 @@ class Usuario {
     }
 
     getBookNames(){
-        const nombreLibros = this.libros[0];
-        return nombreLibros;
-        
-    }
+    
+        const nombreLibros = this.libros
 
+        nombreLibros.forEach(nombres)
+        function nombres(item){
+        console.log(item.nombre)
+        }
+
+
+
+    //students.forEach(myFunction);
+
+    // function myFunction(item) {
+
+    //console.log(item);
+    }
 }
+
+
 const usuario1 = new Usuario ("brian","ramirez",["it","asdasd"]);
 const usuario2= new Usuario ("brian","ramirez",["it","asdasd"]);
 
 // console.log(usuario1.getFullName());
 usuario1.addLibro("it","stephen king")
 usuario1.addLibro("chumbala","las calaveras")
-// usuario1.addMascota("mascota1")
-// usuario1.addMascota("mascota2")
-// // console.log(usuario1.mascotas);
-// // console.log(usuario1.countMascotas());
-console.log(usuario1.getBookNames.libros);
-// // console.log(usuario1.libros);
+// // usuario1.addMascota("mascota1")
+// // usuario1.addMascota("mascota2")
+// // // console.log(usuario1.mascotas);
+// // // console.log(usuario1.countMascotas());
+// // console.log(usuario1.getBookNames);
+// console.log(usuario1.libros);
+console.log(usuario1.getBookNames());
 
 
