83227b1d73adb9121b5e8a8ae9203c74cafa8f5c
Ramirez Brian <tiziano0705@hotmail.com>
2022-08-24 19:27:32 -0300

    correccion

--- Desafio4/server.js
+++ Desafio4/server.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/public'))
 
 
 /*rutas*/
-app.use('/products', routerProductos)
+app.use('/', routerProductos)
 
 /*Server*/ 
 const PORT = 8080;
--- Desafio4/src/routes/productos.routes.js
+++ Desafio4/src/routes/productos.routes.js
@@ -4,35 +4,62 @@ const routerProductos = express.Router()
 /*Database*/
 const DB_PRODUCTOS = []
 
-routerProductos.get('/',(req,res)=>{
+routerProductos.get('/listaproductos',(req,res)=>{
     res.status(200).json(DB_PRODUCTOS)
 })
 
-routerProductos.get('/:id',(req,res)=>{
-    const product = getProductsById(req.params.id)
-    res.send(product)
-})
-
-routerProductos.delete('/:id',(req,res)=>{
-    const productos = deleteProductById(req.params.id)
-    res.send(productos)
-})
-
-routerProductos.post('/', (req, res)=>{
+routerProductos.post('/products', (req, res)=>{
     addProduct(req.body);
     res.status(201).json(DB_PRODUCTOS)
 })
 
 
+routerProductos.get('/mostrarproducto/:id',(req,res)=>{
+    const product = getProductsById(req.params.id)
+    res.send(product)
+})
+
+routerProductos.put('/cambiarprod/:id', (req,res)=>{
+    try{
+    const id = req.params.id
+    const actualizarProduct = DB_PRODUCTOS.find(p => p.id == id);
+    const {title, price} = req.body;
+
+    if(actualizarProduct === undefined){
+    res.status(404).json({error: 'Producto no encontrado'});
+    }else{
+        actualizarProduct.title = title;
+        actualizarProduct.price = price;
+
+        res.status(201).json({msg: 'Producto actualizado'});
+    }
+    }catch (error){
+        res.status(500).json({
+            code:500,
+            msg:`error al obtener ${req.method} ${req.url}`
+        })
+    }
+})
+
+routerProductos.delete('/productos/:id',(req,res)=>{
+    const productos = deleteProductById(req.params.id)
+    res.send(productos)
+})
+
+
+
+
+
 
 /*buscar por el id*/
 function getProductsById(id) {
-    return DB_PRODUCTOS.find(producto => producto.id === id)
+    const asd = DB_PRODUCTOS.find(producto => producto.id == id)
+    return asd
 }
 /*borrar por le id*/
 function deleteProductById(id) {
         const prod = DB_PRODUCTOS
-        const product = prod.findIndex(x => x.id === id)
+        const product = prod.findIndex(x => x.id == id)
         return prod.splice(product, 1);
         
 }
@@ -40,7 +67,7 @@ function deleteProductById(id) {
 /*agregar producto con un nuevo id*/
 function addProduct(producto){
         const productos = DB_PRODUCTOS
-        let newId;0
+        let newId;
 
         if(productos.length == 0){
             newId = 1
